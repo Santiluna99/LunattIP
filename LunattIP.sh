fa343facc26e154fe7ed6f0bd79455334209da2d
santlunaschiavi <santlunaschiavi@gmail.com>
2023-05-23 02:17:09 -0300

    Colores

--- LunattIP.sh
+++ LunattIP.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 figlet -f slant  -c "LunattIP";
-echo "Designed by Santiago Luna" 
+echo -e "\e[38;5;82mDesigned by Santiago Luna\e[0m" 
 for i in {1..255}; do
   timeout 0.3 bash -c "ping -c 1 190.160.0.$i" >/dev/null 2>&1 # cambiar los primeros tres numeros de la direccion IP por la del usuario
   if [ $? -eq 0 ]; then
     echo "El dispositivo con ip 190.160.0.$i está conectado" # cambiar los primeros tres numeros de la direccion IP por la del usuario
   fi
 done
-echo "Fin del escaneo"
+echo -e "\e[1;92mFin del escaneo\e[0m"
