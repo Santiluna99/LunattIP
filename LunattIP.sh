1d3d098fdc0087bb90c586fa6b86ecfce257840b
santlunaschiavi <santlunaschiavi@gmail.com>
2023-05-23 01:34:36 -0300

    First

---
+++ LunattIP.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+figlet -f slant  -c "LunattIP";
+echo "Designed by Santiago Luna" 
+for i in {1..255}; do
+  timeout 0.3 bash -c "ping -c 1 190.160.0.$i" >/dev/null 2>&1 // cambiar direccion IP por la del usuario
+  if [ $? -eq 0 ]; then
+    echo "El dispositivo con ip 190.160.0.$i está conectado"// cambiar direccion IP por la del usuario 
+  fi
+done
+echo "Fin del escaneo"
