b2725c0a3a6e6fa1da5f795480bb0e550cef8f18
santlunaschiavi <santlunaschiavi@gmail.com>
2023-05-23 16:17:23 -0300

    .

--- LunattIP.sh
+++ LunattIP.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 figlet -f slant  -c "LunattIP";
-echo -e "\e[38;5;82mDesigned by Santiago Luna\e[0m" 
-echo -e "\e[1;35mAguarde hasta que lea fin del escaneo\e[0m"
+echo -e "\e[1;35mDesigned by Santiago Luna\e[0m"
+echo -e "\e[38;5;82mAguarde hasta que lea fin del escaneo\e[0m"
 for i in {1..255}; do
   timeout 0.5 bash -c "ping -c 1 190.160.0.$i" >/dev/null 2>&1 # cambiar los primeros tres numeros de la direccion IP por la del usuario
   if [ $? -eq 0 ]; then
