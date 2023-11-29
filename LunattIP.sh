a2733529d0318b2edc59fd29f835132bd303981c
santlunaschiavi <santlunaschiavi@gmail.com>
2023-11-29 15:11:15 -0300

    config de close

--- LunattIP.sh
+++ LunattIP.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
-figlet -f slant  -c "LunattIP";
+trap 'echo -e "\nEscaneo detenido por el usuario"; exit' INT # Captura la señal de interrupción (Ctrl+C) para detener el script
+figlet -f slant -c "LunattIP"
 echo -e "\e[1;35mDesigned by Santiago Luna\e[0m"
 echo -e "\e[38;5;82mAguarde hasta que lea fin del escaneo\e[0m"
+
 for i in {1..255}; do
-  timeout 0.5 bash -c "ping -c 1 190.160.0.$i" >/dev/null 2>&1 # cambiar los primeros tres numeros de la direccion IP por la del usuario
-  if [ $? -eq 0 ]; then
-    echo "El dispositivo con ip 190.160.0.$i está conectado" # cambiar los primeros tres numeros de la direccion IP por la del usuario
+  if timeout 0.5 bash -c "ping -c 1 190.160.0.$i" >/dev/null 2>&1; then
+    # Comprueba si se presionó una tecla
+    if read -t 0.1 -N 1; then
+      echo -e "\nEscaneo detenido por el usuario"
+      exit
+    else
+      echo "El dispositivo con ip 190.160.0.$i está conectado"
+    fi
   fi
 done
+
 echo -e "\e[1;92mFin del escaneo\e[0m"
